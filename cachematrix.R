2ce3999aa297234836bf5bc439fea9d4f7db8e0b
Josh <barnes.joshj@gmail.com>
2016-05-08 22:21:44 -0500

    Adding in comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Created by Joshua Barnes
 
-## Write a short comment describing this function
+## makeCacheMatrix takes in a matrix value. It stores the matrix as a local variable
+## you can call setMatrix or getMatrix to return the saved matrix
+## you can call setInverse or getInverse to return the saved inversed matrix
 
 makeCacheMatrix <- function(myMatrix = matrix()){
     inverseMatrix <- NULL
@@ -29,7 +30,10 @@ makeCacheMatrix <- function(myMatrix = matrix()){
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes in a makeCacheMatrix as a parameter
+## it will attempt to call get InverseMatrx on the passed parameter
+## if no value returns, it will call solve on the provided matrix
+# finally it will then call setInverse passing the inverted matrix
 
 cacheSolve <- function(myMatrix, ...) {
     inverseMatrix <- myMatrix$getInverse()
